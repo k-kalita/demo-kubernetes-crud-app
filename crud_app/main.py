82360be04c76551c8eaa5df487907c2a3a8563fc
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-01-15 11:27:21 +0100

    Dockerfile for fastapi app

--- crud_app/main.py
+++
@@ -1,8 +0,0 @@
-import uvicorn
-from app.fastapi_app import app
-
-APP_HOST = "127.0.0.1"
-APP_PORT = 8000
-
-if __name__ == "__main__":
-    uvicorn.run('main:app', host=APP_HOST, port=APP_PORT, reload=True)
