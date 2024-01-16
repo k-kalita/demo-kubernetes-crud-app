85dbb4b0320323751e6a3d2170e658f7d7b039b9
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2024-01-16 21:26:16 +0100

    views for viewing with your eyeballs (you can indeed view them)

--- crud_app/app/main.py
+++ crud_app/app/main.py
@@ -5,10 +5,13 @@ from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
 from databases import Database
-from pydantic import BaseModel
+from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from hashlib import sha256
 
 from .db_connect import get_db
+
+
+# ----------------------------------------- app config ----------------------------------------- #
 
 APP_ROOT_PATH = Path(__file__).parent
 app = FastAPI()
@@ -20,6 +23,8 @@ templates = Jinja2Templates(directory=APP_ROOT_PATH / "templates")
 app.mount("/static", StaticFiles(directory=APP_ROOT_PATH / "static"), name="static")
 
 
+# ----------------------------------------- validation ----------------------------------------- #
+
 async def validate(db: Database, username: str, password: str) -> None:
     expected_hash = await db.fetch_one(
         "SELECT password_hash FROM `User` WHERE username = :username",
@@ -27,67 +32,60 @@ async def validate(db: Database, username: str, password: str) -> None:
     )
     if expected_hash is None:
         raise HTTPException(status_code=400, detail="User does not exist")
-    if sha256(password.encode()).hexdigest() != expected_hash:
+    if sha256(password.encode()).hexdigest() != expected_hash[0]:
         raise HTTPException(status_code=403, detail="wrong password")
 
 
-class PostModel(BaseModel):
-    username: str
-    password: str
-    title: str
-    content: str
+async def validation_response(db: Database, username: str, password: str) -> JSONResponse | None:
+    try:
+        await validate(db, username, password)
+    except HTTPException as e:
+        return JSONResponse({"status_code": e.status_code,
+                             "status": "failure",
+                             "message": e.detail})
+    return None
 
 
-class PostDeleteModel(BaseModel):
-    username: str
-    password: str
-    id_: int
+# ------------------------------------------ querying ------------------------------------------ #
+
+async def get_user_id(db: Database, username: str) -> int:
+    user_id = await db.fetch_one("SELECT id FROM `User` WHERE username = :username",
+                                 {"username": username})
+    if user_id is None:
+        raise HTTPException(status_code=400, detail="User does not exist")
+    return user_id[0]
 
 
-class UserModel(BaseModel):
-    username: str
-    password: str
-    first_name: str
-    last_name: str
-    email: str
+# ----------------------------------------- post crud ------------------------------------------ #
 
-
-@app.get("/")
-async def home(request: Request):
-    return templates.TemplateResponse("create_post.html", {"request": request})
-
-
-@app.post("/test")
-async def test(request: Request):
-    data = await request.form()
-    return JSONResponse({"status": "success", "message": "Hello World"})
-
-
-@app.get("/view/{username}")
-async def view(username: int):
+@app.get("/view/post/{id_}")
+async def view_post(id_: int):
     ...
+
+
+@app.get("/create/post")
+async def create_post(request: Request):
+    return templates.TemplateResponse("create_post.html", {"request": request})
 
 
 @app.post("/create/post")
 async def create_post(req: Request, db: Database = Depends(get_db)):
     data = await req.form()
+    response = await validation_response(db, data.get('username'), data.get('password'))
+    if response is not None:
+        return response
+    author_id = await get_user_id(db, data.get('username'))
+
     try:
-        await validate(db, data.get('username'), data.get('password'))
-    except HTTPException as e:
-        return JSONResponse({"status_code": e.status_code,
-                             "status": "failure",
-                             "message": e.detail})
-
-    author_id = await db.fetch_one(
-        "SELECT id FROM `User` WHERE username = :username",
-        {"username": data.get('username')}
-    )
-    author_id = author_id[0]
-
-    await db.execute(
-        "INSERT INTO `Post`(title, content, author_id) VALUES (:title, :content, :author_id)",
-        {'title': data.get('title'), 'content': data.get('content'), 'author_id': author_id}
-    )
+        await db.execute(
+            "INSERT INTO `Post`(title, content, author_id)"
+            "VALUES (:title, :content, :author_id)",
+            {'title': data.get('title'),
+             'content': data.get('content'),
+             'author_id': author_id}
+        )
+    except SQLAlchemyError as e:
+        return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
 
     return JSONResponse({"status_code": 200, "status": "success", "message": "Post created"})
 
@@ -95,25 +93,25 @@ async def create_post(req: Request, db: Database = Depends(get_db)):
 @app.post("/delete/post")
 async def delete_post(req: Request, db: Database = Depends(get_db)):
     data = await req.form()
+    response = await validation_response(db, data.get('username'), data.get('password'))
+    if response is not None:
+        return response
+    author_id = await get_user_id(db, data.get('username'))
+
     try:
-        await validate(db, data.get('username'), data.get('password'))
-    except HTTPException as e:
-        return JSONResponse({"status_code": e.status_code,
-                             "status": "failure",
-                             "message": e.detail})
-
-    author_id = await db.fetch_one(
-        "SELECT id FROM `User` WHERE username = :username",
-        {"username": data.get('username')}
-    )
-    author_id = author_id[0]
-
-    await db.execute(
-        "DELETE FROM `Post` WHERE id = :id_ AND author_id = :author_id",
-        {"id_": data.get('id_'), "author_id": author_id}
-    )
+        await db.execute("DELETE FROM `Post` WHERE id = :id_ AND author_id = :author_id",
+                         {"id_": data.get('id_'), "author_id": author_id})
+    except SQLAlchemyError as e:
+        return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
 
     return JSONResponse({"status_code": 200, "status": "success", "message": "Post deleted"})
+
+
+# ----------------------------------------- user crud ------------------------------------------ #
+
+@app.get("/view/{username}")
+async def view_user(username: int):
+    ...
 
 
 @app.get("/create/user")
@@ -124,36 +122,38 @@ def create_user(request: Request):
 @app.post("/create/user")
 async def create_user(req: Request, db: Database = Depends(get_db)):
     data = await req.form()
-
     username = data.get('username')
     password = data.get('password')
     first_name = data.get('first_name')
     last_name = data.get('last_name')
     email = data.get('email')
 
-    out = await db.fetch_all("SELECT id FROM `User` WHERE username = :username",
-                             {'username': username})
-
-    if len(out) > 0:
-        return HTTPException(status_code=400, detail="User already exists")
-
-    await db.execute("INSERT INTO `User`(username, password_hash, name, last_name, email) "
-                     "VALUES (:username, :password_hash, :first_name, :last_name, :email)",
-                     {'username': username, 'password_hash': sha256(password.encode()).hexdigest(),
-                      'first_name': first_name, 'last_name': last_name, 'email': email})
+    try:
+        await db.execute(
+            "INSERT INTO `User`(username, password_hash, name, last_name, email) "
+            "VALUES (:username, :password_hash, :first_name, :last_name, :email)",
+            {'username': username, 'password_hash': sha256(password.encode()).hexdigest(),
+             'first_name': first_name, 'last_name': last_name, 'email': email}
+        )
+    except IntegrityError as e:
+        return JSONResponse({"status_code": 400, "status": "failure", "message": str(e)})
+    except SQLAlchemyError as e:
+        return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
 
     return JSONResponse({"status_code": 200, "status": "success", "message": "User created"})
 
 
 @app.post("/delete/user")
-async def delete_user(req: UserModel, db: Database = Depends(get_db)):
-    out = await db.fetch_all("SELECT id FROM `User` WHERE username = :username",
-                             {'username': req.username})
+async def delete_user(req: Request, db: Database = Depends(get_db)):
+    data = await req.form()
+    response = await validation_response(db, data.get('username'), data.get('password'))
+    if response is not None:
+        return response
 
-    if len(out) == 0:
-        return HTTPException(status_code=400, detail="User does not exist")
-
-    await db.execute("DELETE FROM `User` WHERE username = :username",
-                     {'username': req.username})
+    try:
+        await db.execute("DELETE FROM `User` WHERE username = :username",
+                         {"username": data.get('username')})
+    except SQLAlchemyError as e:
+        return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
 
     return JSONResponse({"status_code": 200, "status": "success", "message": "User deleted"})
