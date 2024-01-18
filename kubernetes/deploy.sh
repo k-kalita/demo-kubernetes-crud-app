e427c4d6a39d5985c16782c446278c12fa33ddc4
k-kalita <krzysztof.kalita@student.uj.edu.pl>
2024-01-18 17:48:51 +0100

    add secrets

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,7 +1,8 @@
+kubectl apply -f db-configmap.yaml
+kubectl apply -f db-secret.yaml
+
 kubectl apply -f db-deploy.yaml
 kubectl apply -f db-service.yaml
-kubectl apply -f db-configmap.yaml
+
 kubectl apply -f crudapp-deploy.yaml
 kubectl apply -f crudapp-service.yaml
-
-#kubectl port-forward --address 0.0.0.0 -n default service/crudapp-service 30080:80
