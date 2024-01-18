189c268f55f4f88d782739f76dac878d36e32817
Mateusz Kadula <mateusz.kadula@icloud.com>
2024-01-18 23:17:41 +0100

    clean commit

--- crud_app/app/db_connect.py
+++ crud_app/app/db_connect.py
@@ -1,5 +1,5 @@
 import os
-from mysql.connector.aio import connect
+from mysql.connector.aio import connect, MySQLConnection
 
 DB_HOST = os.getenv("MYSQL_HOST")
 DB_USER = os.getenv("MYSQL_USER")
@@ -7,16 +7,38 @@ DB_PASSWORD = os.getenv("MYSQL_PASSWORD")
 DB_NAME = os.getenv("MYSQL_DB")
 DB_PORT = os.getenv("MYSQL_PORT")
 
-# if not DB_PORT:
-#     DB_PORT = '3306'
-
 if not all([DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT]):
     raise Exception("Please set the environment variables for the database")
 
-DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-print(DATABASE_URL)
+
+class DatabaseWrapper:
+
+    def __init__(self, db: MySQLConnection, cursor: MySQLConnection.cursor):
+        self._db = db
+        self.cursor = cursor
+
+    async def commit(self):
+        await self._db.commit()
 
 
-async def get_db():
-    db = await connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
-    return db
+class DatabaseConnection:
+
+    def __init__(self):
+        self.db = None
+        self.cursor = None
+        self.wrapper = None
+
+    @staticmethod
+    async def get_db():
+        db = await connect(host=DB_HOST, user=DB_USER, password=DB_PASSWORD, database=DB_NAME)
+        return db
+
+    async def __aenter__(self) -> DatabaseWrapper:
+        self.db = await self.get_db()
+        self.cursor = await self.db.cursor()
+        self.wrapper = DatabaseWrapper(self.db, self.cursor)
+        return self.wrapper
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        await self.cursor.close()
+        await self.db.close()
--- crud_app/app/main.py
+++ crud_app/app/main.py
@@ -4,11 +4,10 @@ from pathlib import Path
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi.templating import Jinja2Templates
-from mysql.connector.aio.connection import MySQLConnection
 from sqlalchemy.exc import IntegrityError, SQLAlchemyError
 from starlette.staticfiles import StaticFiles
 
-from .db_connect import get_db
+from .db_connect import DatabaseConnection, DatabaseWrapper
 
 # ----------------------------------------- app config ----------------------------------------- #
 
@@ -24,19 +23,15 @@ app.mount("/static", StaticFiles(directory=APP_ROOT_PATH / "static"), name="stat
 
 # ----------------------------------------- validation ----------------------------------------- #
 
-async def validate(db: MySQLConnection, username: str, password: str) -> None:
-    cur = await db.cursor()
-
+async def validate(wrapper: DatabaseWrapper, username: str, password: str) -> None:
     try:
-        await cur.execute(
+        await wrapper.cursor.execute(
             "SELECT password_hash FROM `User` WHERE username = %s",
             (username,)
         )
-        expected_hash = (await cur.fetchone())[0]
+        expected_hash = (await wrapper.cursor.fetchone())[0]
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
-    finally:
-        await cur.close()
 
     if expected_hash is None:
         raise HTTPException(status_code=400, detail="User does not exist")
@@ -44,9 +39,9 @@ async def validate(db: MySQLConnection, username: str, password: str) -> None:
         raise HTTPException(status_code=403, detail="wrong password")
 
 
-async def validation_response(db, username: str, password: str) -> JSONResponse | None:
+async def validation_response(wrapper: DatabaseWrapper, username: str, password: str) -> JSONResponse | None:
     try:
-        await validate(db, username, password)
+        await validate(wrapper.cursor, username, password)
     except HTTPException as e:
         return JSONResponse({"status_code": e.status_code,
                              "status": "failure",
@@ -56,19 +51,15 @@ async def validation_response(db, username: str, password: str) -> JSONResponse 
 
 # ------------------------------------------ querying ------------------------------------------ #
 
-async def get_user_id(db, username: str) -> int:
-    cur = await db.cursor()
+async def get_user_id(wrapper: DatabaseWrapper, username: str) -> int:
     try:
-        await cur.execute("SELECT id FROM `User` WHERE username = %s",
-                          (username,))
-        if await cur.fetchone() is None:
+        await wrapper.cursor.execute("SELECT id FROM `User` WHERE username = %s",
+                                     (username,))
+        if await wrapper.cursor.fetchone() is None:
             raise IndexError
-        user_id = (await cur.fetchone())[0]
+        user_id = (await wrapper.cursor.fetchone())[0]
     except (SQLAlchemyError, IndexError) as e:
         return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
-    finally:
-        await cur.close()
-        await db.close()
 
     if user_id is None:
         raise HTTPException(status_code=400, detail="User does not exist")
@@ -89,51 +80,43 @@ async def create_post(request: Request):
 
 @app.post("/create/post")
 async def create_post(req: Request):
-    db = await get_db()
     data = await req.form()
-    response = await validation_response(db, data.get('username'), data.get('password'))
-    if response is not None:
-        db.close()
-        return response
-    author_id = await get_user_id(db, data.get('username'))
 
-    cur = await db.cursor()
-    try:
-        await cur.execute(
-            "INSERT INTO `Post`(title, content, author_id)"
-            "VALUES (%s, %s, %s)",
-            (data.get('title'), data.get('content'), author_id)
-        )
-        await db.commit()
-    except SQLAlchemyError as e:  # TODO: check if this is the right exception
-        return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
-    finally:
-        await cur.close()
-        await db.close()
+    async with DatabaseConnection() as wr:
+        response = await validation_response(wr, data.get('username'), data.get('password'))
+        if response is not None:
+            return response
+        author_id = await get_user_id(wr, data.get('username'))
+
+        try:
+            await wr.cursor.execute(
+                "INSERT INTO `Post`(title, content, author_id)"
+                "VALUES (%s, %s, %s)",
+                (data.get('title'), data.get('content'), author_id)
+            )
+            await wr.commit()
+        except SQLAlchemyError as e:  # TODO: check if this is the right exception
+            return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
 
     return JSONResponse({"status_code": 200, "status": "success", "message": "Post created"})
 
 
 @app.post("/delete/post")
 async def delete_post(req: Request):
-    db = await get_db()
     data = await req.form()
-    response = await validation_response(db, data.get('username'), data.get('password'))
-    if response is not None:
-        db.close()
-        return response
-    author_id = await get_user_id(db, data.get('username'))
 
-    cur = await db.cursor()
-    try:
-        await cur.execute("DELETE FROM `Post` WHERE id = %s AND author_id = %s",
-                          (data.get('id'), author_id))
-        db.commit()
-    except SQLAlchemyError as e:  # TODO: check if this is the right exception
-        return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
-    finally:
-        await cur.close()
-        await db.close()
+    async with DatabaseConnection() as wr:
+        response = await validation_response(wr, data.get('username'), data.get('password'))
+        if response is not None:
+            return response
+        author_id = await get_user_id(wr, data.get('username'))
+
+        try:
+            await wr.cursor.execute("DELETE FROM `Post` WHERE id = %s AND author_id = %s",
+                                    (data.get('id'), author_id))
+            await wr.commit()
+        except SQLAlchemyError as e:  # TODO: check if this is the right exception
+            return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
 
     return JSONResponse({"status_code": 200, "status": "success", "message": "Post deleted"})
 
@@ -152,7 +135,6 @@ def create_user(request: Request):
 
 @app.post("/create/user")
 async def create_user(req: Request):
-    db = await get_db()
     data = await req.form()
     username = data.get('username')
     password = data.get('password')
@@ -160,43 +142,36 @@ async def create_user(req: Request):
     last_name = data.get('last_name')
     email = data.get('email')
 
-    cur = await db.cursor()
-    try:
-        await cur.execute(
-            "INSERT INTO `User`(username, password_hash, name, last_name, email) "
-            "VALUES (%s, %s, %s, %s, %s)",
-            (username, sha256(password.encode()).hexdigest(), first_name, last_name, email)
-        )
-        await db.commit()
-    except IntegrityError as e:
-        return JSONResponse({"status_code": 400, "status": "failure", "message": str(e)})
-    except SQLAlchemyError as e:
-        return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
-    finally:
-        await cur.close()
-        await db.close()
+    async with DatabaseConnection() as wr:
+        try:
+            await wr.cursor.execute(
+                "INSERT INTO `User`(username, password_hash, name, last_name, email) "
+                "VALUES (%s, %s, %s, %s, %s)",
+                (username, sha256(password.encode()).hexdigest(), first_name, last_name, email)
+            )
+            await wr.commit()
+        except IntegrityError as e:
+            return JSONResponse({"status_code": 400, "status": "failure", "message": str(e)})
+        except SQLAlchemyError as e:
+            return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
 
     return JSONResponse({"status_code": 200, "status": "success", "message": "User created"})
 
 
 @app.post("/delete/user")
 async def delete_user(req: Request):
-    db = await get_db()
     data = await req.form()
-    response = await validation_response(db, data.get('username'), data.get('password'))
-    if response is not None:
-        db.close()
-        return response
 
-    cur = await db.cursor()
-    try:
-        await cur.execute("DELETE FROM `User` WHERE username = %s",
-                          (data.get('username'),))
-        db.commit()
-    except SQLAlchemyError as e:
-        return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
-    finally:
-        await cur.close()
-        await db.close()
+    async with DatabaseConnection() as wr:
+        response = await validation_response(wr, data.get('username'), data.get('password'))
+        if response is not None:
+            return response
+
+        try:
+            await wr.cursor.execute("DELETE FROM `User` WHERE username = %s",
+                                    (data.get('username'),))
+            await wr.commit()
+        except SQLAlchemyError as e:
+            return JSONResponse({"status_code": 500, "status": "failure", "message": str(e)})
 
     return JSONResponse({"status_code": 200, "status": "success", "message": "User deleted"})
